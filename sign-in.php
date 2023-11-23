69c9944efeea3139795eaceebfeccb692b2520b9
Kirupan Inpathas <114853337+kirupan10@users.noreply.github.com>
2023-11-23 13:17:25 +0530

    image

--- sign-in.php
+++ sign-in.php
@@ -144,11 +144,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
               </div>
             </div>
             <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" 
-              style="background-image: url('assets/img/bg-image.jpg')">
-                <span class="mask bg-gradient-primary opacity-1"></span>
-              </div>
-            </div>
+    <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" 
+        style="background-image: url('assets/img/bg-image.jpg')">
+        <span class="mask bg-gradient-primary opacity-1"></span>
+    </div>
+</div>
+
           </div>
         </div>
       </div>
